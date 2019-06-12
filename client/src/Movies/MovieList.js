df768e06939de309801fb1d2d10f99a4c16b211c
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-06-11 20:04:31 -0700

    Stretch goal complete. DRY Refactor of Movie.js and MovieList.js

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {NavLink} from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -25,33 +25,9 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     );
   }
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
-  return (
-    <NavLink to={`/movies/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-    </NavLink>
-  );
 }
